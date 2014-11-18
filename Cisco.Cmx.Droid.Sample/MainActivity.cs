938571c1246a17c2778ffd7b9d258cea02068b60
Geir Sagberg <geir.sagberg@gmail.com>
2014-11-18 08:55:40 +0100

    Added sample project (WIP)

---
+++ Cisco.Cmx.Droid.Sample/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Cisco.Cmx.Droid.Sample
+{
+    [Activity(Label = "Cisco.Cmx.Droid.Sample", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+        }
+    }
+}
+
+
